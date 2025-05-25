a9aac41e820217ba39ac39c47480364094b83f47
Taf Makura <tafmakura@gmail.com>
2025-05-25 15:16:16 +0200

    Update arsol-wps-packet-system-styles-sync.php

--- arsol-wps-packet-system-styles-sync.php
+++ arsol-wps-packet-system-styles-sync.php
@@ -4,7 +4,7 @@ Plugin Name: Arsol WP Snippets Packet: Arsol System Style Sync
 Requires Plugins: arsol-wp-snippets
 Plugin URI: https://example.com/arsol-wp-snippets-packet-example
 Description: A collection of useful snippets for WordPress development.
-Version: 1.0.2
+Version: 1.0.3
 Author: Taf Makura
 Author URI: https://example.com
 License: GPL2
