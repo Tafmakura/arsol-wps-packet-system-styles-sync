f93dbf9f92d79125e876a1d879af52e161716ebb
Taf Makura <tafmakura@gmail.com>
2025-05-25 15:14:00 +0200

    Update

--- functions/functions-php-snippets-loader.php
+++ functions/functions-php-snippets-loader.php
@@ -12,3 +12,14 @@ function arsol_wps_packet_system_styles_add_my_example_php($php_options) {
     );
     return $php_options;
 }
+
+
+// Add example.php
+add_filter('arsol_wp_snippets_php_addon_files', 'arsol_wps_packet_system_styles_twooo');
+function arsol_wps_packet_system_styles_twooo($php_options) {
+    $php_options['my-example-php'] = array(
+        'name' => 'My Example PHP file',
+        'file' => __DIR__ . '/../snippets/php/twooo.php'
+    );
+    return $php_options;
+}
