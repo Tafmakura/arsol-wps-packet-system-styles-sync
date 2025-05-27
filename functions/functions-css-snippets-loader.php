d8015d9413e543927c1ebdf14cc19c503fad7be3
Taf Makura <tafmakura@gmail.com>
2025-05-27 10:09:27 +0200

    Added System Files

--- functions/functions-css-snippets-loader.php
+++ functions/functions-css-snippets-loader.php
@@ -3,12 +3,36 @@
  * CSS Snippets Loader for Arsol WP Snippets
  */
 
-// Add example.css
-add_filter('arsol_wp_snippets_css_addon_files', 'arsol_wps_packet_system_styles_add_my_example_css');
-function arsol_wps_packet_system_styles_add_my_example_css($css_options) {
+// Table System Styles
+add_filter('arsol_wp_snippets_css_addon_files', 'arsol_wps_packet_system_styles_add_table_styles');
+function arsol_wps_packet_system_styles_add_table_styles($css_options) {
     $css_options[] = array(
-        'name' => 'My Example CSS file',
-        'file' => plugin_dir_url(__FILE__) . '../snippets/css/example.css',
+        'name' => 'System: Table Styles',
+        'file' => plugin_dir_url(__FILE__) . '../snippets/css/table-system-styles.css',
+        'context' => 'frontend',
+        'position' => 'header'
+    );
+    return $css_options;
+}
+
+// Button System Styles
+add_filter('arsol_wp_snippets_css_addon_files', 'arsol_wps_packet_system_styles_add_button_styles');
+function arsol_wps_packet_system_styles_add_button_styles($css_options) {
+    $css_options[] = array(
+        'name' => 'System: Button Styles',
+        'file' => plugin_dir_url(__FILE__) . '../snippets/css/button-system-styles.css',
+        'context' => 'frontend',
+        'position' => 'header'
+    );
+    return $css_options;
+}
+
+// Form System Styles
+add_filter('arsol_wp_snippets_css_addon_files', 'arsol_wps_packet_system_styles_add_form_styles');
+function arsol_wps_packet_system_styles_add_form_styles($css_options) {
+    $css_options[] = array(
+        'name' => 'System: Form Styles',
+        'file' => plugin_dir_url(__FILE__) . '../snippets/css/form-system-styles.css',
         'context' => 'frontend',
         'position' => 'header'
     );
