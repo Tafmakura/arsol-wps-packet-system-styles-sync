04601ab04425b35261422f90c5028edfc825abda
Taf Makura <tafmakura@gmail.com>
2025-05-27 16:38:47 +0200

    Uopdate

--- functions/functions-php-snippets-loader.php
+++ functions/functions-php-snippets-loader.php
@@ -22,3 +22,26 @@ function arsol_wps_packet_system_styles_add_bricks_php($php_options) {
     );
     return $php_options;
 }
+
+// Add Test1 PHP
+add_filter('arsol_wp_snippets_php_addon_files', 'arsol_wps_packet_system_styles_add_test1_php', 15);
+function arsol_wps_packet_system_styles_add_test1_php($php_options) {
+    $php_options[] = array(
+        'name' => 'Test1 Logger',
+        'file' => __DIR__ . '/../test1.php',
+        'priority' => 15
+    );
+    return $php_options;
+}
+
+// Add Test2 PHP
+add_filter('arsol_wp_snippets_php_addon_files', 'arsol_wps_packet_system_styles_add_test2_php', 5);
+function arsol_wps_packet_system_styles_add_test2_php($php_options) {
+    $php_options[] = array(
+        'name' => 'Test2 Logger',
+        'file' => __DIR__ . '/../test2.php',
+        'priority' => 5
+    );
+    return $php_options;
+}
+
---
+++ test1.php
@@ -0,0 +1,3 @@
+<?php
+error_log('Test1.php executed at ' . date('Y-m-d H:i:s'));
+?> 
