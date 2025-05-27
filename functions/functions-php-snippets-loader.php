0c84e2460744c25261f0b937ad9b0458e41d61ca
Taf Makura <tafmakura@gmail.com>
2025-05-27 17:31:26 +0200

    Update functions-php-snippets-loader.php

--- functions/functions-php-snippets-loader.php
+++ functions/functions-php-snippets-loader.php
@@ -19,28 +19,7 @@ function arsol_wps_packet_system_styles_add_bricks_php($php_options) {
     $php_options[] = array(
         'name' => 'Plugin: Bricks Builder',
         'file' => __DIR__ . '/../snippets/php/plugin-bricks.php'
-    );
-    return $php_options;
-}
-
-// Add Test1 PHP
-//add_filter('arsol_wp_snippets_php_addon_files', 'arsol_wps_packet_system_styles_add_test1_php', 15);
-function arsol_wps_packet_system_styles_add_test1_php($php_options) {
-    $php_options[] = array(
-        'name' => 'Test1 Logger',
-        'file' => __DIR__ . '/../snippets/php/test1.php',
-        'priority' => 5
-    );
-    return $php_options;
-}
-
-// Add Test2 PHP
-//add_filter('arsol_wp_snippets_php_addon_files', 'arsol_wps_packet_system_styles_add_test2_php', 5);
-function arsol_wps_packet_system_styles_add_test2_php($php_options) {
-    $php_options[] = array(
-        'name' => 'Test2 Logger',
-        'file' => __DIR__ . '/../snippets/php//test2.php',
-        'priority' => 15
+        'priority' => 99996
     );
     return $php_options;
 }
