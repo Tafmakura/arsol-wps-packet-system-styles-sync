75dcc016b546b8fa55f20dbc015d88b8c3898d83
Taf Makura <tafmakura@gmail.com>
2025-05-27 12:07:26 +0200

    Update

--- functions/functions-php-snippets-loader.php
+++ functions/functions-php-snippets-loader.php
@@ -12,3 +12,13 @@ function arsol_wps_packet_system_styles_add_account_funds_php($php_options) {
     );
     return $php_options;
 }
+
+// Add Bricks PHP
+add_filter('arsol_wp_snippets_php_addon_files', 'arsol_wps_packet_system_styles_add_bricks_php');
+function arsol_wps_packet_system_styles_add_bricks_php($php_options) {
+    $php_options[] = array(
+        'name' => 'Plugin: Bricks Builder',
+        'file' => __DIR__ . '/../snippets/php/plugin-bricks.php'
+    );
+    return $php_options;
+}
