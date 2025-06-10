1c6ac19d611edfc8cc1273964a055b6abfd35c93
Taf Makura <tafmakura@gmail.com>
2025-06-10 19:36:33 +0200

    Update

--- snippets/php/test1.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Output HTML content with hook priority
-echo '<div class="test1-container">';
-echo '<h2>Test 1 Content</h2>';
-echo '<p>This content is being rendered from test2.php at ' . date('Y-m-d H:i:s') . '</p>';
-
-?>
--- snippets/php/test2.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Output HTML content with hook priority
-echo '<div class="test2-container">';
-echo '<h2>Test 2 Content</h2>';
-echo '<p>This content is being rendered from test2.php at ' . date('Y-m-d H:i:s') . '</p>';
-
-echo '</div>';
-?>
